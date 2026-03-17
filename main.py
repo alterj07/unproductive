d39d0970676c301f0fc47afc9f008a3d45da652f
Jayden Chun <jayden.h.chun@gmail.com>
2026-03-17 11:19:47 -0500

    added a gitignore + added an audio file that will play on detection of a phone

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
 import cv2 as cv
 from ultralytics import YOLO
+import subprocess
+
+model = YOLO('yolov8n.pt')
+TARGET_OBJECT = "cell phone"
+
+
+def isTargetPresent(frame, target: str, threshold: float = 0.5) -> bool:
+    results = model(frame, verbose=False)
+
+    for result in results:
+        for box in result.boxes:
+            confidence = float(box.conf)
+            class_id = int(box.cls)
+            class_name = model.names[class_id]
+
+            if class_name == target and confidence >= threshold:
+                return True
+
+    return False
 
 def runDetection():
-    model = YOLO('yolov8n.pt')
     cam = cv.VideoCapture(0)
     while True:
         ret, frame = cam.read()
+
         if not ret:
             print("Failed to grab frame")
             break
+        
         results = model(frame)
         annotated_frame = results[0].plot()
-        cv.imshow('YOLOv8 Detection', annotated_frame)
+        cv.imshow('Unproductive?', annotated_frame)
+        
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
+        
+        if isTargetPresent(frame, TARGET_OBJECT):
+            subprocess.run(['afplay', 'pipe.wav'])
+
+
     cam.release()
     cv.destroyAllWindows()
 
